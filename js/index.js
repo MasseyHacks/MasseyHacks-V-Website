bd50ff31df68c64f113761169562fabca2ea5cdd
Ryan <ryanzhang0112@gmail.com>
2017-12-11 23:26:50 -0500

    Mail button started

--- js/index.js
+++ js/index.js
@@ -12,4 +12,28 @@ $(document).ready(function(){
             $("#navmain").removeClass("navbar-fixed-top");
         }
     });
+
+    var emailBox = $("#mce-EMAIL");
+    var subSubmit = $("#mc-embedded-subscribe");
+    var dcID = "us17";
+    var apikey = "9a3694a4119716258581ccb8f0f0a6ad-us17";
+    var listUniqueId = "f7aab2d91d";
+
+    var xhr = new XMLHttpRequest();
+
+    subSubmit.click(function () {
+        var email = emailBox.val();
+        xhr.open('POST', 'https://' + dcID + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/');
+        xhr.setRequestHeader("Authorization", "Basic " + btoa("ryanz34:"+apikey));
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        console.log(xhr.send(JSON.stringify({
+            'email_address': email,
+            'status': 'subscribed',
+            'merge_fields': {
+                'FNAME': 'testing',
+                'LNAME': 'testing'
+            }
+        })));
+    })
 });
