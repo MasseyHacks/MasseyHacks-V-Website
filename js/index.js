62c307fbb683221b54afa8fd9d7be942d7741291
ryan <ryanzhang0112@gmail.com>
2018-01-01 15:49:01 -0500

    textbox clearing

--- js/index.js
+++ js/index.js
@@ -10,12 +10,12 @@ $(document).ready(function () {
     });
 
     function sub (email) {
-        if (email != '') {
+        if (email.val() != '') {
             $.ajax({
                 url: './php/emailSub.php',
                 type: 'POST',
                 data: {
-                    email: email
+                    email: email.val()
                 },
                 success: function (msg) {
                     if (msg == "success") {
@@ -24,6 +24,8 @@ $(document).ready(function () {
                             title: 'Subscribed!',
                             html: 'Get ready for the latest updates about MasseyHacks IV!'
                         });
+                        email.val("");
+
                     } else if (msg == 'invalid') {
                         swal(
                             'Oops...',
@@ -59,13 +61,13 @@ $(document).ready(function () {
     $('#mce-EMAIL').keydown(function (e) {
         if (e.which == 13) {
             e.preventDefault();
-            var email = $('#mce-EMAIL').val()
+            var email = $('#mce-EMAIL');
             sub(email);
         }
     });
 
     $('#mc-embedded-subscribe').click(function () {
-        var email = $('#mce-EMAIL').val()
+        var email = $('#mce-EMAIL');
         sub(email);
     });
 
