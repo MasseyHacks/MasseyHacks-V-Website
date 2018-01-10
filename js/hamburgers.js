7867a4aabf44c440623f1a7ebef5f96c60a55eaf
henrytwo <henryt.inc@gmail.com>
2018-01-10 09:37:54 -0500

    Repair merge

---
+++ js/hamburgers.js
@@ -0,0 +1,39 @@
+//handles everything to do with hamburger
+$(document).ready(function(){
+    toggleHamburger();
+    $(".hamburger").on("click",function(){
+        toggleOverlay();
+    });
+
+    $('a.overlayLink').click(function () {
+        toggleOverlay();
+    });
+});
+
+$(window).resize(function(){
+    toggleHamburger();
+});
+
+function toggleOverlay() {
+    $("#navham").toggleClass("is-active");
+    if($("#navham").hasClass("is-active")){
+        $(".overlay").css({visibility: "visible"});
+        $("html").css({"overflow-y": "hidden"});
+    } else{
+        $(".overlay").css({visibility: "hidden"});
+        $("html").css({"overflow-y": "visible"});
+    }
+}
+
+function toggleHamburger(){
+    //toggles hamburger based on window width
+    if($(window).width() <= 767){
+        $("#navham").removeClass("hidden");
+        $("#navleft").hide();
+        $("#navright").hide();
+    } else{
+        $("#navham").addClass("hidden");
+        $("#navleft").show();
+        $("#navright").show();
+    }
+}
---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    $('.js-navbar-link').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - $("#mainnav").height()
+        }, 300, 'swing', function () {
+            //window.location.hash = target;
+        });
+    });
+});
