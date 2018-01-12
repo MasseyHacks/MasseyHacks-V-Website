baf44550c3fb4fc40777926b51a81f8305a5f2f0
henrytwo <henryt.inc@gmail.com>
2018-01-11 23:01:42 -0500

    Optimize loading

---
+++ js/mail.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    function sub (email) {
+        if (email.val() != '') {
+            $.ajax({
+                url: './php/emailSub.php',
+                type: 'POST',
+                data: {
+                    email: email.val()
+                },
+                success: function (msg) {
+                    if (msg == "success") {
+                        swal({
+                            type: 'success',
+                            title: 'Subscribed!',
+                            html: 'Get ready for the latest updates about MasseyHacks IV! <br>(' + email.val() + ')'
+                        });
+                        email.val("");
+
+                    } else if (msg == 'invalid') {
+                        swal(
+                            'Oops...',
+                            'Invalid email address.',
+                            'error'
+                        );
+                    } else if (msg == 'asubbed'){
+                        swal(
+                            'Oops...',
+                            'You are already subscribed. <br>(' + email.val() + ')',
+                            'error'
+                        );
+                    } else {
+                        swal(
+                            'Oops...',
+                            'Something went wrong! Please try again later.',
+                            'error'
+                        );
+                    }
+                },
+                error: function () {
+                    swal(
+                        'Oops...',
+                        'Something went wrong! Please try again later.',
+                        'error'
+                    );
+                }
+
+            });
+        }
+    }
+
+    $('#mce-EMAIL').keydown(function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            var email = $('#mce-EMAIL');
+            sub(email);
+        }
+    });
+
+    $('#mc-embedded-subscribe').click(function () {
+        var email = $('#mce-EMAIL');
+        sub(email);
+    });
+});
