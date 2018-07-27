dc0603f172c012ddbc1bf3a2dce0de6caeb6c046
Henry T <henryt.inc@gmail.com>
2018-07-27 14:43:39 -0400

    Fixed cover scaling

--- js/index.js
+++ js/index.js
@@ -21,33 +21,6 @@ $(document).ready(function () {
         toggleOverlay();
     });
 
-    /*
-    var opacity;
-
-    function updateScroll() {
-
-        var newOpacity;
-        var dist = $("#about").offset().top - $(window).scrollTop();
-
-        if (dist > 0) {
-            newOpacity = 1 * (1 - dist / $("#about").offset().top);
-        }
-        else {
-            newOpacity = 1;
-        }
-
-        if (newOpacity != opacity) {
-            opacity = newOpacity;
-            $("#mainnav").css("background", "rgba(10, 25, 57, " + newOpacity + ")");
-        }
-    }
-
-    var throttled = _.throttle(updateScroll, 60);
-
-    $(window).resize(throttled);
-    $(window).scroll(throttled);
-    updateScroll(); */
-
     var bgresize = function () {
 
         var windowz = $(window);
