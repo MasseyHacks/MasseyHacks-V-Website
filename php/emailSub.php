3cf3fa7d8785672f8dcb7716feb82806a649b194
David Hui <BlazingAsher@users.noreply.github.com>
2018-09-22 10:20:19 -0400

    added configuration options to mailchimp script

--- php/emailSub.php
+++ php/emailSub.php
@@ -1,13 +1,14 @@
 <?php
 include('MailChimp.php');
+require_once('config.php');
 use \DrewM\MailChimp\MailChimp;
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $MailChimp = new MailChimp('1558fda43ee0c637951831962a296212-us17');
+        $MailChimp = new MailChimp($_CONFIG["mailchip_key"]);
 
-        $list_id = 'df5627faa3';
+        $list_id = $_CONFIG["mailchip_list_id"];
 
         $subscriber_hash = $MailChimp->subscriberHash($email);
         $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
