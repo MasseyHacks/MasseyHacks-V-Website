80f7c6a336f377c967c6fcced8552eda539f1ef6
Henry T <henryt.inc@gmail.com>
2018-10-20 23:37:57 -0400

    Preparing for early deploy

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ $(document).ready(function () {
         var $target = $(target);
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 50
+            'scrollTop': $target.offset().top
         }, 500, 'swing', function () {
 
         });
