7c7d47b4d258ed7d57c822fdafad245c165e77d1
Henry T <henryt.inc@gmail.com>
2019-03-07 17:11:58 -0500

    Added schedule

--- js/index-r3.js
+++ js/index-r3.js
@@ -159,3 +159,29 @@ function toggleOverlay() {
         $("html").css({"overflow-y": "visible"});
     }
 }
+
+$.ajax({
+    type: 'GET',
+    url: 'schedule.json',
+    dataType: 'json',
+    success: function (data) {
+        var exp = '';
+        for (var i = 0; i < data.length; i++) {
+            var d = data[i];
+            var xOffset = 100 + 200 * d['xs'];
+            var width = (d['xe'] - d['xs']) * 200;
+            var yOffset = 95 + 80 * d['y'];
+            exp += '<div style="left: ' + xOffset + 'px; top: ' + yOffset + 'px; width: ' + width + 'px !important; background-color: ' + d['color'] + ';" class="schedule-label">' +
+                '<div class="inner-label">' +
+                '<p class="label-caption"><b>' + d['caption'] + '</b></p>' +
+                '<p class="label-description">' + d['start'] + ' - ' + d['end'] + ' | ' + d['location'] + '</p>' +
+                '</div></div>';
+        }
+        $('#label-container').html(exp);
+
+    },
+    error: function (data) {
+        $('#schedule-frame').html('<b><h2>Server error occurred<br>Unable to get schedule</h2></b>');
+    }
+})
+
